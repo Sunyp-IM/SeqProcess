eef5bae67e551301eae6cfad3edbbbbcc8c33ab8
Sunyp-IM <yeping.sun.2022@gmail.com>
2023-03-02 10:32:55 -0500

    Add files via upload

---
+++ extractGbFeature.py
@@ -0,0 +1,34 @@
+from Bio import SeqIO
+from Bio import SeqUtils
+from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint as IP
+import pandas as pd
+
+path = 'H:/projects/vaccina/seq/'
+infile = path + 'sequence.gb'
+# record = SeqIO.parse(infile, "genbank")
+record = SeqIO.read(open(infile, "r"), "genbank")
+#rec = next(record)
+recList = []
+featureList = []
+for feature in record.features:
+    if feature.type == 'CDS':
+        featureDic = feature.qualifiers
+        if 'gene' in featureDic:
+            gene = featureDic['gene']
+        else:
+            gene = featureDic['locus_tag']
+        product = featureDic['product']
+        start = feature.location.start.position
+        end = feature.location.end.position
+        seq = record.seq[start:end]
+        if seq[:3] != 'ATG':
+            seq = seq.reverse_complement()
+        length = end - start
+        translation = feature.qualifiers['translation'][0]
+        mw = round(SeqUtils.molecular_weight(translation, seq_type='protein')/1000, 1)
+        isoP = round(IP(translation).pi())
+        featureList.append([gene, product, start, end, seq, length, translation, mw, isoP])
+        df = pd.DataFrame(featureList)
+        df.columns = ['Gene', 'Product', 'Start', 'End', 'DNA Sequence', 'DNA length', 'Protein Sequence',
+                      'Molecular weight', 'Isoelectric point']
+        df.to_excel(path + 'vacciniaGenome.xlsx')
